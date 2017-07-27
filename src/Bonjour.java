65c07a77f29c3b4ce8a54e0908ce829d9f223108
Angelo Basso <angelo67170@gmail.com>
2017-07-27 12:06:44 +0200

    ajout différent fichier java du premier TP

---
+++ src/Bonjour.java
@@ -0,0 +1,16 @@
+//ceci est un commentaire sur une seul ligne
+/*
+Ceci 
+est
+un
+commentaire
+sur
+plusieurs
+lignes
+*/
+public class Bonjour{
+	public static void main(String args[]){
+
+	System.out.println("Bonjour tout le monde");
+	}
+}
---
+++ src/Bonjour2.java
@@ -0,0 +1,5 @@
+public class Bonjour2{
+	public static void main(String args[]){
+		System.out.println("Bonjour "+args[0]);
+	}
+}
